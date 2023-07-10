6082448d14d22b06bde88d3b89d99adfd57b7ccf
Thomas R McNutt <pachown@gmail.com>
2023-07-10 04:08:21 -0600

    Added service to serve mock hero data
    Observed the new service so async data will still work

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +8,24 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {
+    
+  }
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+  
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  
 }
