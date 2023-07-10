49620f6bbd0c3116baae5d4b7c71731db1c41fcd
Thomas R McNutt <pachown@gmail.com>
2023-07-10 04:17:27 -0600

    Added message service
    implemented message service to state when the heroes were fetched and when each new hero is selected in the list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  constructor(private heroService: HeroService) {
-    
-  }
-  heroes: Hero[] = [];
+export class HeroesComponent implements OnInit{
   selectedHero?: Hero;
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
   
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero name=${hero.name}`);
   }
   
   getHeroes(): void {
@@ -24,8 +27,5 @@ export class HeroesComponent {
         .subscribe(heroes => this.heroes = heroes);
   }
   
-  ngOnInit(): void {
-    this.getHeroes();
-  }
   
 }
